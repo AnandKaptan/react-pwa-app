b1fb318d785b00e45cd82bfd01ad42ffcac9006a
Anand Kaptan <AnandKaptan@hotmail.com>
2021-04-20 21:50:48 +0530

    update 3

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 /* thisisasample */
 
 function App() {
+  const fruits = ['apple', 'mango', 'chikoo'];
   return (
     <div className='App'>
       <header className='App-header'>App Header</header>
+      <ul>
+        {fruits.map((f) => (
+          <li>{f}</li>
+        ))}
+      </ul>
     </div>
   );
 }
